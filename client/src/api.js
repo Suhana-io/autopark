cfe01f9ee04a68a42c44f671ff4526deafbc3554
suhana <suhanashaik448@gmail.com>
2026-03-29 23:39:17 +0530

    Fix API URL direct

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || 
-                'https://autopark-backend-506o.onrender.com'
+const API_URL = 'https://autopark-backend-506o.onrender.com'
 
 export default API_URL
