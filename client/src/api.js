59cab80a65a4276a66b4909de024f052f6e30935
suhana <suhanashaik448@gmail.com>
2026-03-29 23:28:31 +0530

    Update API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,4 @@
-const API_URL = 'https://autopark-backend-506o.onrender.com'
+const API_URL = import.meta.env.VITE_API_URL || 
+                'https://autopark-backend-506o.onrender.com'
 
 export default API_URL
